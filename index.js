e569af65e4dc184d30ddaab61536e1c321a2f12b
kj <kristoferjoseph@gmail.com>
2019-01-29 09:58:53 -0800

    Optimizes rule look up. Adds additive test.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const css = require('css')
 module.exports = function Tamer (stylesheet) {
   stylesheet = stylesheet || ''
+  let copy = {}
+  let styles = css.parse(stylesheet)
+  styles.stylesheet.rules.map(r => {
+    r.selectors.map(s => {
+      copy[s] = r
+    })
+  })
   return function tame (extensions) {
     extensions = extensions || {}
     let components = Object.keys(extensions)
-    let styles = css.parse(stylesheet)
-    styles.stylesheet.rules.map(r => {
-      r.selectors.map(s => {
-        components.map(c => {
-          extensions[c].map(e => {
-            if (e === s) {
-              r.selectors.unshift(c)
-            }
-          })
-        })
+    components.map(c => {
+      extensions[c].map(e => {
+        copy[e].selectors.unshift(c)
       })
     })
     return css.stringify(styles)
--- test.js
+++ test.js
@@ -38,11 +38,41 @@ const expected = `
   font-size: 1rem;
 }
 `
+const expected2 = `
+.link,
+.button,
+.padding0 {
+  padding: 1rem;
+}
+.link,
+.button,
+.color-p1 {
+  color: white;
+}
+.button,
+.background-p2 {
+  background-color: red;
+}
+.background-a1 {
+  background-color: teal;
+}
+.link,
+.font-size0 {
+  font-size: 1rem;
+}
+`
 const extensions = {
   '.button': [
     '.padding0',
     '.color-p1',
     '.background-p2'
+  ]
+}
+const extensions2 = {
+  '.link': [
+    '.padding0',
+    '.color-p1',
+    '.font-size0'
   ]
 }
 function strip (s) {
@@ -51,6 +81,7 @@ function strip (s) {
 let pass = true
 try {
   assert.equal(strip(tame(extensions)), strip(expected))
+  assert.equal(strip(tame(extensions2)), strip(expected2))
   pass = true
 } catch (err) {
   pass = false
